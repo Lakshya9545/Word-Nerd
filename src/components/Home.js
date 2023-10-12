5d7e1bc28f7dea259ee9c1b1641ca5dcfaed4264
Lakshya9545 <pandyalakshya04@gmail.com>
2023-10-12 16:19:23 +0530

    word...

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,42 +6,44 @@ function Home() {
   const [wordData, setWordData] = useState([]);
 
   async function fetchRandomWords() {
-    const response = await axios.get(
-      "https://random-word-api.herokuapp.com/word?number=5"
-    );
-    const randomWords = response.data;
+    try {
+      const response = await axios.get(
+        "https://random-word-api.herokuapp.com/word?number=5"
+      );
+      const randomWords = response.data;
 
-    const definitionResponses = await Promise.all(
-      randomWords.map((randomWord) => {
-        return axios.get(
-          `https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`
-        );
-      })
-    );
+      const definitionResponses = await Promise.all(
+        randomWords.map(async (randomWord) => {
+          return axios.get(
+            `https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`
+          );
+        })
+      );
 
-    const wordDataWithExplanations = [];
-    for (let i = 0; i < randomWords.length; i++) {
-      const definitionResponse = definitionResponses[i];
-      const word = randomWords[i];
-      const meanings = definitionResponse.data[0]?.meanings;
-      const explanation = meanings && meanings[0]?.definitions[0]?.definition;
+      const wordDataWithExplanations = [];
+      for (let i = 0; i < randomWords.length; i++) {
+        const definitionResponse = definitionResponses[i];
+        const word = randomWords[i];
+        const meanings = definitionResponse.data[0]?.meanings;
+        const explanation = meanings && meanings[0]?.definitions[0]?.definition;
 
-      wordDataWithExplanations.push({ word, explanation });
+        wordDataWithExplanations.push({ word, explanation });
+      }
+
+      setWordData(wordDataWithExplanations);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-
-    setWordData(wordDataWithExplanations);
   }
 
   useEffect(() => {
     fetchRandomWords();
   }, []);
 
-
-  
   return (
     <>
       <div className="box">
-        <h1 className="title"> Word Nerd </h1>
+        <h1 className="title">Word Nerd</h1>
 
         {wordData.map((data, index) => (
           <div className="vocab" key={index}>
